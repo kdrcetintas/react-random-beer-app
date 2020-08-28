0b33c89ef4579efb7e51cafd79dd8239b39e7e4f
Kadir Çetintaş <mert@mail.com>
2020-08-28 11:31:45 +0300

    last

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Requirements:
 - npm
 - yarn
 
-To run the app install:
+To install:
 
 ```bash
 git clone https://github.com/kdrcetintas/react-random-beer-app.git
@@ -20,8 +20,9 @@ Description:
 * cors-anwhere demo link used at the api requests due to cors error
 * tailwindcss used for the ui
 * redux for state management
-* redux-thunk for async action creators
+* redux-thunk for async action creators, no custom middleware
+* axios for http requests
 * all api results mapped under /types folder
-* state/prop types mapped as inline in the component files
-* detail page has two way data read structure, from the global state or from the api. If there is no data at the global state for specific brewery, it's will read from the api and save the global state
 * big components is seperated at /components folder
+* state/prop types mapped as inline in the component/view files
+* brewery detail page has two way data read structure, from the global state or from the api. If there is no data at the global state for specific brewery, it's will read from the api and save the global state
--- src/views/AboutPage.tsx
+++ src/views/AboutPage.tsx
@@ -14,6 +14,21 @@ export default function AboutPage() {
                 linkedinLink="github@kdrcetintas"
                 linkedinText="linkedin@kdrcetintas"
             />
+            <hr />
+            <div className="mt-3 bg-gray-100 rounded-lg p-6 shadow">
+                <span className="font-bold">Description</span>
+                <ul className="list-disc ml-5">
+                    <li>cors-anwhere demo link used at the api requests due to cors error</li>
+                    <li>tailwindcss used for the ui</li>
+                    <li>redux for state management</li>
+                    <li>redux-thunk for async action creators, no custom middleware</li>
+                    <li>axios for http requests</li>
+                    <li>all api results mapped under /types folder</li>
+                    <li>big components is seperated at /components folder</li>
+                    <li>state/prop types mapped as inline in the component/view files</li>
+                    <li>brewery detail page has two way data read structure, from the global state or from the api. If there is no data at the global state for specific brewery, it's will read from the api and save the global state</li>
+                </ul>
+            </div>
         </div>
     )
 }
